2d2b80efe824e6e143a502ff5eb53eec7821a9e7
sourabh pisal <pisal.sourabh@gmail.com>
2023-04-09 22:01:41 +0000

    Adding pipeline notifications

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -6,12 +6,18 @@ import {
 } from 'aws-cdk-lib/pipelines';
 import {Construct} from 'constructs';
 import {PipelineStage} from './pipeline-stage';
+import {Topic} from 'aws-cdk-lib/aws-sns';
+import {EmailSubscription} from 'aws-cdk-lib/aws-sns-subscriptions';
+import {
+  DetailType,
+  NotificationRule,
+} from 'aws-cdk-lib/aws-codestarnotifications';
 
 export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'AwsBillingAlertsPipeline', {
+    const codePipeline = new CodePipeline(this, 'AwsBillingAlertsPipeline', {
       pipelineName: 'AwsBillingAlertsPipeline',
       synth: new ShellStep('synth', {
         input: CodePipelineSource.gitHub(
@@ -22,12 +28,31 @@ export class PipelineStack extends Stack {
       }),
     });
 
-    pipeline.addStage(
+    codePipeline.addStage(
       new PipelineStage(this, 'prod', {
         env: {
           region: 'us-east-1',
         },
       })
     );
+
+    codePipeline.buildPipeline();
+
+    const pipelineTopic = new Topic(this, 'AwsBillingAlertsPipelineTopic');
+    pipelineTopic.addSubscription(
+      new EmailSubscription('{{resolve:ssm:email}}')
+    );
+
+    new NotificationRule(this, 'AwsBillingAlertsPipelineNotification', {
+      detailType: DetailType.BASIC,
+      events: [
+        'codepipeline-pipeline-pipeline-execution-failed',
+        'codepipeline-pipeline-pipeline-execution-canceled',
+        'codepipeline-pipeline-pipeline-execution-succeeded',
+        'codepipeline-pipeline-pipeline-execution-superseded',
+      ],
+      source: codePipeline.pipeline,
+      targets: [pipelineTopic],
+    });
   }
 }
